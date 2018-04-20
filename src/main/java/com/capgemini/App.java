86ee31168a108b063feac163160820cd0e8d75a3
lucadolstra <38104438+lucadolstra@users.noreply.github.com>
2018-04-20 09:48:18 +0200

    Setup Git

---
+++ src/main/java/com/capgemini/App.java
@@ -0,0 +1,14 @@
+package com.capgemini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" +
+                "a random change" );
+    }
+}
